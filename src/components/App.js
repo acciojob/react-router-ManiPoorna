fae9ccf9a1795f691b38a8c25d0ffdb4c94d84f7
Mani Poorna <ndmprao2001@gmail.com>
2023-10-18 10:33:02 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter, HashRouter, Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <HashRouter>
+        <Link to="/" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
+      
     </div>
   )
 }
